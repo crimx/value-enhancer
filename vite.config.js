ae9d4957ea5ea03498269034c8fa6c4c6bc39d00
crimx <straybugs@gmail.com>
2022-08-28 01:00:41 +0800

    chore: remove vitest

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 /* eslint-env node */
 
 import path from "path";
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
 
 export default defineConfig(({ mode }) => {
   const isProd = mode === "production";
