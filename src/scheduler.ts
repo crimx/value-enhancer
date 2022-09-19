2d3578ebad308f7c54de1188b5a4d2b317ef3faf
crimx <straybugs@gmail.com>
2022-09-19 23:08:11 +0800

    refactor: remove async feature deps

--- src/scheduler.ts
+++ src/scheduler.ts
@@ -4,22 +4,21 @@ export type Task<TValue = any> = (value: TValue) => void;
 
 const nextTick = /*#__PURE__*/ Promise.resolve();
 const subsSet = new Set<Subscribers>();
-let pending: Promise<void> | false | undefined;
+let pending: Promise<void> | false;
 
-export async function schedule<TValue>(
-  subs: Subscribers<TValue>
-): Promise<void> {
+export function schedule<TValue>(subs: Subscribers<TValue>): void {
   subsSet.add(subs);
-  if (!pending) {
-    await (pending = nextTick);
-    for (const subs of subsSet) {
-      subs.exec_("s0");
-    }
-    subsSet.clear();
-    pending = false;
-  }
+  pending = pending || nextTick.then(flush);
 }
 
 export function cancelTask(subs: Subscribers): void {
   subsSet.delete(subs);
+}
+
+function flush() {
+  for (const subs of subsSet) {
+    subs.exec_("s0");
+  }
+  pending = false;
+  subsSet.clear();
 }
