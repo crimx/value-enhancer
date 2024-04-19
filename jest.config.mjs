cbe27efb24a5a714b13ec5973372af160cc9a762
CRIMX <straybugs@gmail.com>
2024-04-19 23:11:07 +0800

    chore: update jest script

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,7 +1,6 @@
 export default {
   preset: "ts-jest",
   testEnvironment: "node",
-  collectCoverage: true,
   collectCoverageFrom: ["src/**/*.ts", "!src/index.ts"],
   coverageReporters: ["clover", "json", "lcov", "text"],
 };
