bc34499b5848faf1b9ff94b2adebae1ceea247c8
CRIMX <straybugs@gmail.com>
2024-04-29 09:36:27 +0800

    ci: fix ci jest testing

--- jest.config.mjs
+++ jest.config.mjs
@@ -3,4 +3,7 @@ export default {
   testEnvironment: "node",
   collectCoverageFrom: ["src/**/*.ts", "!src/index.ts"],
   coverageReporters: ["clover", "json", "lcov", "text"],
+  moduleNameMapper: {
+    "^value-enhancer$": "<rootDir>/src/index.ts",
+  },
 };
